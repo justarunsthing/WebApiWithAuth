142f6469e28cdc01ccee8df41d8e04dbd2bce5ae
justarunsthing <justarunsthing@outlook.com>
2025-12-25 09:50:17 +0000

    Added TaskerItemDto model

---
+++ Models/TaskerItemDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiWithAuth.Models
+{
+    public class TaskerItemDto
+    {
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "Every task must have a name")]
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
