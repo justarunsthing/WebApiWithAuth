ebbf0ad375b8058f9e334e70f5390032a0d36777
justarunsthing <justarunsthing@outlook.com>
2025-12-25 09:47:12 +0000

    Added TaskerItem model

---
+++ Models/TaskerItem.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiWithAuth.Models
+{
+    public class TaskerItem
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+
+        [Required (ErrorMessage = "Every task must have a name")]
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; } = false;
+
+        // Foreign key relationship
+        public virtual IdentityUser User { get; set; }
+    }
+}
