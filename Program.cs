4fc6c2f72c909ffc441a7ac89b9ea51e45fb6351
justarunsthing <justarunsthing@outlook.com>
2025-12-26 10:14:13 +0000

    Configure connection string

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using WebApiWithAuth.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
