99eb66aca5dba0e82ebb68396074c1000ccc270b
justarunsthing <justarunsthing@outlook.com>
2025-12-28 10:32:06 +0000

    Authorize get tasker items action

--- Controllers/TaskerItemsController.cs
+++ Controllers/TaskerItemsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +29,7 @@ namespace WebApiWithAuth.Controllers
         }
 
         // GET: api/TaskerItems
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TaskerItem>>> GetTaskerItems()
         {
