111db803cbf8fa977d9bfd2b47e08155b8ff9449
justarunsthing <justarunsthing@outlook.com>
2025-12-28 10:31:08 +0000

    Added TaskerItemExtensions

---
+++ Extensions/TaskerItemExtensions.cs
@@ -0,0 +1,14 @@
+﻿using WebApiWithAuth.Models;
+
+namespace WebApiWithAuth.Extensions
+{
+    public static class TaskerItemExtensions
+    {
+        public static TaskerItemDto ToDTO(this TaskerItem item ) => new()
+        {
+            Id = item.Id,
+            Name = item.Name,
+            IsComplete = item.IsComplete
+        };
+    }
+}
