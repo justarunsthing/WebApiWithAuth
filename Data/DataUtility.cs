5ffbca64750988d6b0bd4dd2494974e7af10df57
justarunsthing <justarunsthing@outlook.com>
2025-12-30 09:28:18 +0000

    Added DataUtility class

---
+++ Data/DataUtility.cs
@@ -0,0 +1,33 @@
+﻿using Npgsql;
+
+namespace WebApiWithAuth.Data
+{
+    public static class DataUtility
+    {
+        public static string? GetConnectionString(IConfiguration configuration)
+        {
+            string? connectionString = configuration.GetConnectionString("DefaultConnection");
+            string? databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            Uri databaseUri = new(databaseUrl);
+            string[] userInfo = databaseUri.UserInfo.Split(':');
+
+            NpgsqlConnectionStringBuilder builder = new()
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
+            };
+
+            return builder.ToString();
+        }
+    }
+}
